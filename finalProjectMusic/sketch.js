cd71436d95ab6e20756c2923c4af1bca16268393
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-05-15 15:15:17 -0600

    fixed screen sized

--- finalProjectMusic/sketch.js
+++ finalProjectMusic/sketch.js
@@ -5,14 +5,16 @@ let state;
 let musicFile;
 let testSong1;
 let testSong1Thumbnail;
-
+let backgroundMusic;
 function setup(){
   state = "warning";
-  createCanvas(windowWidth,windowHeight);
+  let canvas = createCanvas(800, 800);
+  canvas.position(windowWidth/4, 0);
 }
 function preload(){
   testSong1 = loadSound("assets/testSong1.wav");
   testSong1Thumbnail = loadImage("assets/5hoursThumbnail.jpg");
+  backgroundMusic = loadSound("assets/beginningbackgroundMusic.wav");
 }
 function draw(){
   background(0);
@@ -23,35 +25,37 @@ function stateScreens(){
 
   if (state === "warning"){
     background(0);
+    textAlign(CENTER);
     fill(0,255,0);
-    textSize(windowWidth/28);
-    text("O N L Y  W O R K S  I N   F U L L S C R E E N ",windowWidth/10,windowHeight/2);
-    text("C L I C K  T O  C O N T I N U E",windowWidth/4,windowHeight/1.5);
+    textSize(30);
+    text("L O O K S  B E S T  I N   F U L L S C R E E N ",400,windowHeight/2);
+    text("C L I C K  T O  C O N T I N U E",400,windowHeight/1.5);
     if (mouseIsPressed){
+      backgroundMusic.play();
+      backgroundMusic.amp(1);
       state = 1;
     }
   }
   if (state === 1 /*Welcome Screen*/){
-    let rectWidth = windowWidth/8;
-    let rectLength = windowHeight/16;
     background(0);
     fill(0, 255, 0);
     textAlign(CENTER);
     textFont("Agency FB");
-    textSize(windowWidth/25);
-    text("Welcome to Noah's Music Visualizer",windowWidth/2,windowHeight/4);
+    textSize(60);
+    text("Welcome to Noah's Music Visualizer",400,200);
     rectMode(CENTER);
-    textSize(windowWidth/35);
-    text("UPLOAD",windowWidth/2,windowHeight/1.5);
-    text("USE PRESET SONGS",windowWidth/2,windowHeight/1.9);
-    // if (mouseX >= windowWidth/25 || mouseX <= windowWidth/25 ){
-    //   if (mouseIsPressed){
-    //     state = 2;
+    textSize(30);
+
+    //text("UPLOAD",400,windowHeight/1.5);
+    text("SONG SELECTION",400,500);
+    if (mouseX <= 400+77.5 && mouseX >= 400-77.5 && mouseY <=487+12.5 && mouseY >= 487-12.5){
+      if (mouseIsPressed){
+        state = 2;
       }
     }
-  //}
+  }
   if (state === 2/*Preset Songs*/){
     background(0);
 
   }
-//}
+}
